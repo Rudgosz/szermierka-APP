a55608f928e81d2c839eb6eb3f5888b07576faee
Rudgosz <tryton.maciek@gmail.com>
2024-07-02 23:45:27 +0200

    random color triangles

--- screens/Training.js
+++ screens/Training.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 
 const screenHeight = Dimensions.get('window').height;
@@ -8,19 +8,61 @@ const triangleWidthConst = 10;
 const triangleOffset = 30;
 
 const rotationAngle = Math.atan(screenWidth / screenHeight); // Radians
-const bottomWidthConst = Math.sqrt((Math.pow(screenHeight/2, 2) + Math.pow(screenWidth/2, 2))) * 1;
+const bottomWidthConst = Math.sqrt((Math.pow(screenHeight / 2, 2) + Math.pow(screenWidth / 2, 2))) * 1;
+
+const defaultTriangleColors = {
+  blueTriangle: '#b5b5b5',
+  redTriangle: '#b5b5b5',
+  greenTriangle: '#b5b5b5',
+  yellowTriangle: '#b5b5b5',
+  purpleTriangle: '#b5b5b5',
+  orangeTriangle: '#b5b5b5',
+  pinkTriangle: '#b5b5b5',
+  cyanTriangle: '#b5b5b5'
+};
+
+const backgroundColor = '#f0f0f0';
 
 const TriangleScreen = () => {
+  const [triangleColors, setTriangleColors] = useState(defaultTriangleColors);
+
+  useEffect(() => {
+    const triangleNames = Object.keys(defaultTriangleColors);
+    let interval;
+
+    const changeColor = () => {
+      const randomTriangle = triangleNames[Math.floor(Math.random() * triangleNames.length)];
+      const originalColor = defaultTriangleColors[randomTriangle];
+
+      setTriangleColors((prevColors) => ({
+        ...prevColors,
+        [randomTriangle]: 'yellow'
+      }));
+
+      setTimeout(() => {
+        setTriangleColors((prevColors) => ({
+          ...prevColors,
+          [randomTriangle]: originalColor
+        }));
+      }, 1000);
+    };
+
+    interval = setInterval(changeColor, 1500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.blueTriangle} />
-      <View style={styles.redTriangle} />
-      <View style={styles.greenTriangle} />
-      <View style={styles.yellowTriangle} />
-      <View style={styles.purpleTriangle} />
-      <View style={styles.orangeTriangle} />
-      <View style={styles.pinkTriangle} />
-      <View style={styles.cyanTriangle} />
+      
+      <View style={[styles.triangle, styles.blueTriangle, { borderBottomColor: triangleColors.blueTriangle }]} />
+      <View style={[styles.triangle, styles.redTriangle, { borderBottomColor: triangleColors.redTriangle }]} />
+      <View style={[styles.triangle, styles.greenTriangle, { borderBottomColor: triangleColors.greenTriangle }]} />
+      <View style={[styles.triangle, styles.yellowTriangle, { borderBottomColor: triangleColors.yellowTriangle }]} />
+      <View style={[styles.triangle, styles.purpleTriangle, { borderBottomColor: triangleColors.purpleTriangle }]} />
+      <View style={[styles.triangle, styles.orangeTriangle, { borderBottomColor: triangleColors.orangeTriangle }]} />
+      <View style={[styles.triangle, styles.pinkTriangle, { borderBottomColor: triangleColors.pinkTriangle }]} />
+      <View style={[styles.triangle, styles.cyanTriangle, { borderBottomColor: triangleColors.cyanTriangle }]} />
       <View style={styles.grayRectangle} />
     </View>
   );
@@ -31,187 +73,93 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    backgroundColor: backgroundColor,
+  },
+  triangle: {
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: screenWidth / triangleWidthConst,
+    borderRightWidth: screenWidth / triangleWidthConst,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
   },
   blueTriangle: {
-    position: 'absolute',
     top: screenHeight / 2 + triangleOffset,
     left: (screenWidth / 2) - (screenWidth / triangleWidthConst),
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
     borderBottomWidth: screenHeight / 2,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'blue',
     transform: [{ rotate: '0deg' }],
-    marginBottom: 0,
   },
   redTriangle: {
-    position: 'absolute',
     top: triangleOffset,
     left: (screenWidth / 2) - (screenWidth / triangleWidthConst),
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
     borderBottomWidth: screenHeight / 2,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'red',
     transform: [{ rotate: '180deg' }],
-    marginBottom: screenHeight,
   },
   greenTriangle: {
-    position: 'absolute',
     top: screenHeight / 2 + triangleOffset - screenWidth / 4,
-    left: (screenWidth / 4 - screenWidth / triangleWidthConst) + screenWidth/2,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
+    left: (screenWidth / 4 - screenWidth / triangleWidthConst) + screenWidth / 2,
     borderBottomWidth: screenWidth / 2,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'green',
     transform: [{ rotate: '-90deg' }],
-    marginBottom: 0,
   },
   yellowTriangle: {
-    position: 'absolute',
     top: screenHeight / 2 + triangleOffset - screenWidth / 4,
     left: screenWidth / 4 - screenWidth / triangleWidthConst,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
     borderBottomWidth: screenWidth / 2,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'yellow',
     transform: [{ rotate: '90deg' }],
-    marginBottom: 0,
   },
-  
   purpleTriangle: {
-    position: 'absolute',
-    top: screenHeight/2 - bottomWidthConst/2 + triangleOffset,
-    left: screenWidth/2 - screenWidth / triangleWidthConst,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
+    top: screenHeight / 2 - bottomWidthConst / 2 + triangleOffset,
+    left: screenWidth / 2 - screenWidth / triangleWidthConst,
     borderBottomWidth: bottomWidthConst,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'purple',
     transform: [
-      {rotate: `${rotationAngle}rad` },
-      {translateX: 0},
-      {translateY: bottomWidthConst/2},
-      
+      { rotate: `${rotationAngle}rad` },
+      { translateX: 0 },
+      { translateY: bottomWidthConst / 2 },
     ],
-    marginBottom: 0,
   },
   orangeTriangle: {
-    position: 'absolute',
-    top: screenHeight/2 - bottomWidthConst/2 + triangleOffset,
-    left: screenWidth/2 - screenWidth / triangleWidthConst,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
+    top: screenHeight / 2 - bottomWidthConst / 2 + triangleOffset,
+    left: screenWidth / 2 - screenWidth / triangleWidthConst,
     borderBottomWidth: bottomWidthConst,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'orange',
     transform: [
-      {rotate: `-${rotationAngle}rad` },
-      {translateX: 0},
-      {translateY: bottomWidthConst/2},
-      
+      { rotate: `-${rotationAngle}rad` },
+      { translateX: 0 },
+      { translateY: bottomWidthConst / 2 },
     ],
-    marginBottom: 0,
   },
   pinkTriangle: {
-    position: 'absolute',
-    top: screenHeight/2 - bottomWidthConst/2 + triangleOffset,
-    left: screenWidth/2 - screenWidth / triangleWidthConst,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
+    top: screenHeight / 2 - bottomWidthConst / 2 + triangleOffset,
+    left: screenWidth / 2 - screenWidth / triangleWidthConst,
     borderBottomWidth: bottomWidthConst,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'pink',
     transform: [
-      {rotate: `-${rotationAngle}rad` },
-      {rotate: '180deg' },
-      {translateX: 0},
-      {translateY: bottomWidthConst/2},
-      
+      { rotate: `-${rotationAngle}rad` },
+      { rotate: '180deg' },
+      { translateX: 0 },
+      { translateY: bottomWidthConst / 2 },
     ],
-    marginBottom: 0,
   },
-
   cyanTriangle: {
-    position: 'absolute',
-    top: screenHeight/2 - bottomWidthConst/2 + triangleOffset,
-    left: screenWidth/2 - screenWidth / triangleWidthConst,
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth / triangleWidthConst,
-    borderRightWidth: screenWidth / triangleWidthConst,
+    top: screenHeight / 2 - bottomWidthConst / 2 + triangleOffset,
+    left: screenWidth / 2 - screenWidth / triangleWidthConst,
     borderBottomWidth: bottomWidthConst,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: 'cyan',
     transform: [
-      {rotate: `${rotationAngle}rad` },
-      {rotate: '180deg' },
-      {translateX: 0},
-      {translateY: bottomWidthConst/2},
-      
+      { rotate: `${rotationAngle}rad` },
+      { rotate: '180deg' },
+      { translateX: 0 },
+      { translateY: bottomWidthConst / 2 },
     ],
-    marginBottom: 0,
   },
-
   grayRectangle: {
     position: 'absolute',
     top: 0,
     left: 0,
-    width: 0,
-    height: 0,
-    backgroundColor: '#f0f0f0',
-    borderStyle: 'solid',
-    borderLeftWidth: screenWidth,
-    borderRightWidth: screenWidth,
-    borderBottomWidth: triangleOffset,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: '#f0f0f0',
-    //transform: [{ rotate: '90deg' }],
-    marginBottom: 0,
+    width: screenWidth,
+    height: triangleOffset,
+    backgroundColor: backgroundColor,
   },
-  
 });
 
 export default TriangleScreen;
